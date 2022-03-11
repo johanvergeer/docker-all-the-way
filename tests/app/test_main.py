ed0cb185347b730a24b45e9802bb9290d57d94e8
Johan Vergeer <johanvergeer@gmail.com>
2022-03-11 15:28:26 +0100

    write test for main endpoint

---
+++ tests/app/test_main.py
@@ -0,0 +1,11 @@
+from app.main import app
+
+from fastapi.testclient import TestClient
+
+client = TestClient(app)
+
+
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello World"}
