a74787adcd5a8ec3d44b5404a7857059439edc6d
Johan Vergeer <johanvergeer@gmail.com>
2022-03-27 10:46:35 +0200

    create initial file with shell aliases

---
+++ aliases.sh
@@ -0,0 +1,19 @@
+COMPOSE_FILES="-f docker-compose.yaml -f docker-compose.run-local.yaml"
+APP_CONTAINER="docker-all-the-way_app_1"
+DB_CONTAINER="docker-all-the-way_db_1"
+
+alias dcup="docker-compose $COMPOSE_FILES up -d"
+alias dcdown="docker-compose $COMPOSE_FILES down"
+
+# Login to the db Docker container
+alias dbshell="docker exec -it $DB_CONTAINER bash"
+# Login to the app Docker container
+alias appshell="docker exec -it $APP_CONTAINER bash"
+
+# Run database migrations in the db container
+alias runmigrations="docker cp db_migrations/. $DB_CONTAINER:/etc/opt/db/ && docker exec $DB_CONTAINER bash /etc/opt/db/run_migrations.sh"
+# Run unit tests in Docker container
+alias test="docker exec $APP_CONTAINER python -m unittest"
+
+# Run poetry commands in Docker container
+alias poetry="docker exec $APP_CONTAINER poetry"
